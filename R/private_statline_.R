baad1392a8f3494cee35f9392b3a82529846de46
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2021-09-30 12:17:58 +0200

    added plot functions

--- R/private_statline_.R
+++ R/private_statline_.R
@@ -7,24 +7,16 @@
     values = !!sym(dvar)
   )
 
-  args <- line$args
-  if (is.null(args$na.rm)) args$na.rm <- TRUE
+  if (is.null(line$args$na.rm)) line$args$na.rm <- TRUE
+
 
   dat_stat <- data %>%
     group_by(case, phase) %>%
-    summarise(stat = do.call(fun, c(list(values), args)))
+    summarise(y = do.call(fun, c(list(values), line$args)))
 
   data <- full_join(data, dat_stat, by = c("case", "phase"))
 
-  p <- geom_line(
-    data = data,
-    aes(x = mt, y = stat, group = phase),
-    linetype = line$line$linetype,
-    color = line$line$colour,
-    size = line$line$size
-  )
-
-  p
+  .statline_geom_phase(data, line$line)
 }
 
 .statline_fixed_first <- function(data, line, dvar, mvar, pvar, fun) {
@@ -35,27 +27,18 @@
     values = !!sym(dvar)
   )
 
-  args <- line$args
-  if (is.null(args$na.rm)) args$na.rm <- TRUE
+  if (is.null(line$args$na.rm)) line$args$na.rm <- TRUE
 
   first_phase <- levels(data$phase)[1]
 
   dat_stat <- data %>%
     filter(phase == first_phase) %>%
     group_by(case) %>%
-    summarise(stat = do.call(fun, c(list(values), args)))
+    summarise(y = do.call(fun, c(list(values), line$args)))
 
   data <- full_join(data, dat_stat, by = c("case"))
 
-  p <- geom_line(
-    data = data,
-    aes(x = mt, y = stat),
-    linetype = line$line$linetype,
-    color = line$line$colour,
-    size = line$line$size
-  )
-
-  p
+  .statline_geom(data, line$line)
 }
 
 .statline_trend <- function(data, line, dvar, mvar, pvar) {
@@ -86,15 +69,7 @@
     data[filter, "y"] <- data$mt[filter] * b + int
   }
 
-  p <- geom_line(
-    data = data,
-    aes(x = mt, y = y, group = phase),
-    linetype = line$line$linetype,
-    color = line$line$colour,
-    size = line$line$size
-  )
-
-  p
+  .statline_geom_phase(data, line$line)
 }
 
 .statline_trendA <- function(data, line, dvar, mvar, pvar) {
@@ -125,15 +100,7 @@
     data$y[filter] <- data$mt[filter] * b + int
   }
 
-  p <- geom_line(
-    data = data,
-    aes(x = mt, y = y),
-    linetype = line$line$linetype,
-    color = line$line$colour,
-    size = line$line$size
-  )
-
-  p
+  .statline_geom(data, line$line)
 }
 
 .statline_moving_average <- function(data, line, dvar, mvar, pvar, fun) {
@@ -144,25 +111,16 @@
     values = !!sym(dvar)
   )
 
-  lag <- line$args$lag
-  if (is.null(lag)) lag <- 1
+  if (is.null(line$args$lag)) line$args$lag <- 1
 
   data$y <- NA
 
   for(case in unique(data$case)) {
     filter <- which(data$case == case)
-    data$y[filter] <- .moving_average(data$values[filter], lag, fun)
+    data$y[filter] <- .moving_average(data$values[filter], line$args$lag, fun)
   }
 
-  p <- geom_line(
-    data = data,
-    aes(x = mt, y = y),
-    linetype = line$line$linetype,
-    color = line$line$colour,
-    size = line$line$size
-  )
-
-  p
+  .statline_geom(data, line$line)
 }
 
 .statline_loreg <- function(data, line, dvar, mvar, pvar) {
@@ -173,25 +131,21 @@
     values = !!sym(dvar)
   )
 
-  f <- line$args$f
-  if (is.null(f)) f <- 0.5
+  if (is.null(line$args$f)) line$args$f <- 0.5
 
   data$y <- NA
 
   for(case in unique(data$case)) {
     filter <- which(data$case == case)
-    data$y[filter] <- lowess(data$values[filter] ~ data$mt[filter], f = f)$y
+    data$y[filter] <-
+      do.call(
+        "lowess",
+        c(list(data$values[filter] ~ data$mt[filter]), line$args)
+      )$y
   }
 
-  p <- geom_line(
-    data = data,
-    aes(x = mt, y = y),
-    linetype = line$line$linetype,
-    color = line$line$colour,
-    size = line$line$size
-  )
+  .statline_geom(data, line$line)
 
-  p
 }
 
 
@@ -204,4 +158,28 @@
     x[i] <- fun(x[(i - xlag):(i + xlag)], na.rm = TRUE)
 
   x
+}
+
+.statline_geom <- function(data, line) {
+
+  geom_line(
+    data = data,
+    aes(x = mt, y = y),
+    linetype = line$linetype,
+    color = line$colour,
+    size = line$size
+  )
+
+}
+
+.statline_geom_phase <- function(data, line) {
+
+  geom_line(
+    data = data,
+    aes(x = mt, y = y, group = phase),
+    linetype = line$linetype,
+    color = line$colour,
+    size = line$size
+  )
+
 }
