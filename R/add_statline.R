fc59f66ec82c45a77ec7e907e7b8ead319b90f42
Jürgen Wilbert <50012671+jazznbass@users.noreply.github.com>
2026-03-07 12:06:56 +0100

    new case argument

--- R/add_statline.R
+++ R/add_statline.R
@@ -6,44 +6,46 @@
 #'
 #' @inheritParams .inherit_scplot
 #' @param stat A character string for defining a statistical line or curve to be
-#'   plotted. Possible values are `"mean"`, `"median"`, `"min"`, `"max"`,
-#'   `"quantile"`, `"sd"`, `"mad"`, `"trend"`, `"trendA"`, `"trendA theil-sen"`,
-#'   `"trendA bisplit"`, `"trendA trisplit"`, `"moving mean"`,
-#'   `"moving median"`, `"loreg"` (local regression using `loess`), and
-#'   `"lowess"` (local regression using `lowess`).
+#'   plotted. Main options are `"mean"`, `"median"`, `"min"`, `"max"`,
+#'   `"quantile"`, `"sd"`, `"mad"`, `"trend"`, `"moving mean"`, `"moving
+#'   median"`, `"loreg"` (local regression using `loess`).
 #' @param phase Either a numeric or a character vector specifying the reference
 #'   phase (see details).
 #' @param label A character string which is used to set the label in a legend.
 #' @param ... additional parameters passed to the statistical function.
-#' @param segmented Logical. If TRUE, the statline is plotted separately for each phase.
-#'   The default is NULL, where sensible settings are applied based on the `stat` and `phase` arguments.
+#' @param segmented Logical. If TRUE, the statline is plotted separately for
+#'   each phase. The default is NULL, where sensible settings are applied based
+#'   on the `stat` and `phase` arguments.
+#' @param case Either a numeric or a character vector specifying the case(s) for which the
+#'   statline is plotted. The default is NULL, which applies the statline to all
+#'   cases.
 #' @details The `phase` argument defines the reference phase for some
-#'   statistical functions (`"median", "mean", "min", "max", "quantile"`). The
-#'   default is `NULL` which calculates and plots statistics for each phase
-#'   separately. The arguments takes a numeric vector (phase number(s)) or a
-#'   character vector (phase name(s)). When more than one phase is defines,
-#'   statistics are based on the combined values of these phases. Various
-#'   methods for an extrapolated *trendA* line exist: `"trendA"` is based on an
-#'   OLS regression, `"trendA theil-sen"` on a nonparametric regression, and
-#'   `"trendA bisplit"` / `"trendA trisplit"` are two median based approaches.
-#'   Some of the functions defined in `stats` have additional arguments. The
-#'   [mean()] function has a trim argument (e.g. `trim = 0.1`). [quantile()] has
-#'   a proportion argument (e.g. `prob = 0.75` for calculating the 75%
-#'   quantile). `moving mean` and `moving median` have a lag argument (e.g. `lag
-#'   = 2`). The local-regression curve function `"lowess"` (or `"loreg"`) has a
-#'   proportion argument (e.g. `f = 0.5`; see [lowess()]) and the
-#'   local-regression curve function `"loess"` has a span argument (e.g. `span =
-#'   0.75`; see [loess()]).
+#'   statistical functions (`"median", "mean", "min", "max", "quantile",
+#'   "trend"`). The default is `NULL` which calculates and plots statistics for
+#'   each phase separately. The arguments takes a numeric vector (phase
+#'   number(s)) or a character vector (phase name(s)). When more than one phase
+#'   is defines, statistics are based on the combined values of these phases.
+#'   `phase = all` will select all phases. Various methods for a *trend* line
+#'   exist that can be set with `method` argument: The default is based on an
+#'   OLS regression, `"theil-sen"` on a nonparametric regression, and
+#'   `"bisplit"` / `"trisplit"` are two median based approaches. Some of the
+#'   functions defined in `stat` have additional arguments. The [mean()]
+#'   function has a trim argument (e.g. `trim = 0.1`). [quantile()] has a
+#'   proportion argument (e.g. `prob = 0.75` for calculating the 75% quantile).
+#'   `moving mean` and `moving median` have a lag argument (e.g. `lag = 2`). The
+#'   local-regression curve function `"lowess"` (or `"loreg"`) has a proportion
+#'   argument (e.g. `f = 0.5`; see [lowess()]) and the local-regression curve
+#'   function `"loess"` has a span argument (e.g. `span = 0.75`; see [loess()]).
 #' @return An object of class `scplot` (see[scplot()]) with changed element
 #'   `statlines`.
 #' @export
 add_statline <- function(object,
                          stat = c("mean", "median", "min", "max", "quantile",
-                                  "sd", "mad",
-                                  "trend", "trendA", "trendA theil-sen",
-                                  "trendA bisplit", "trendA trisplit",
+                                  "sd", "mad", "trend",
                                   "moving mean", "moving median",
-                                  "loreg", "lowess", "loess"),
+                                  "loreg", "lowess", "loess",
+                                  "trendA", "trendA theil-sen",
+                                  "trendA bisplit", "trendA trisplit"),
                          phase = NULL,
                          color = NULL,
                          linewidth = NULL,
@@ -51,12 +53,14 @@ add_statline <- function(object,
                          variable = NULL,
                          label = NULL,
                          segmented = NULL,
+                         case = NULL,
                          ...) {
 
   stat <- match.arg(stat)
 
   if (is.null(variable)) variable <- ".dvar"
 
+  # add line element for statline ----
   line <- list(
     linewidth = linewidth,
     colour = color,
@@ -73,25 +77,52 @@ add_statline <- function(object,
 
   args <- list(...)
 
-  # adjust stat name for trendA variants ----
-  if (!is.null(args$method)) {
-    if (args$method == "bisplit" && stat == "trendA") {
-      stat <- "trendA bisplit"
-      args$method <- NULL
-    }
-    if (args$method == "trisplit" && stat == "trendA") {
-      stat <- "trendA trisplit"
-      args$method <- NULL
-    }
+  # organize arguments for trend stat ----
+
+  if (stat == "trend" && is.null(args$method)) {
+    args$method <- "lm"
   }
-  if (identical(stat, "trendA")) {
-    phase <- 1
+
+  if (stat == "trendA theil-sen") {
+    if (is.null(phase)) phase <- 1
     stat <- "trend"
+    args$method <- "theil-sen"
   }
-  if (identical(stat, "trendA theil-sen")) {
-    args$method == "theil-sen"
-    phase <- 1
+
+  if (stat == "trendA bisplit") {
+    if (is.null(phase)) phase <- 1
     stat <- "trend"
+    args$method <- "bisplit"
+  }
+
+  if (stat == "trendA trisplit") {
+    if (is.null(phase)) phase <- 1
+    stat <- "trend"
+    args$method <- "trisplit"
+  }
+
+  if (stat == "trendA" && (is.null(args$method) || args$method == "lm")) {
+    if (is.null(phase)) phase <- 1
+    stat <- "trend"
+    args$method <- "lm"
+  }
+
+  if (stat == "trendA" && args$method == "bisplit") {
+    if (is.null(phase)) phase <- 1
+    stat <- "trend"
+    args$method <- "bisplit"
+  }
+
+  if (stat == "trendA" && args$method == "trisplit") {
+    if (is.null(phase)) phase <- 1
+    stat <- "trend"
+    args$method <- "trisplit"
+  }
+
+  if (stat == "trendA" && args$method == "theil-sen") {
+    if (is.null(phase)) phase <- 1
+    stat <- "trend"
+    args$method <- "theil-sen"
   }
 
   # set default label ----
@@ -113,10 +144,14 @@ add_statline <- function(object,
       }
     }
 
-    label <- paste0(stat, " ", variable, phase_str)
+    stat_str <- stat
+    if (stat == "trend" && args$method == "theil-sen") stat_str <- "trend theil-sen"
+    if (stat == "trend" && args$method == "bisplit") stat_str <- "trend bisplit"
+    if (stat == "trend" && args$method == "trisplit") stat_str <- "trend trisplit"
+    label <- paste0(stat_str, " ", variable, phase_str)
   }
 
-  # set default segment option for statlines ----
+  # set default segmented option for statlines ----
   if (is.null(segmented)) {
     segmented <- FALSE
     stat_selection <- c("mean", "median", "min", "max", "quantile",
@@ -133,7 +168,8 @@ add_statline <- function(object,
     args = args,
     variable = variable,
     label = label,
-    segmented = segmented
+    segmented = segmented,
+    case = case
   )
 
   object$statlines <- c(object$statlines, list(new))
